d4abf0d3f0248e9103618a2fc0b2cfef2a6e6c92
David Kern <david@mju.io>
2022-08-30 21:36:20 -0700

    setup android ndk project

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
+pub fn main() {
+    println!("Hello, wo3rld!");
 }
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    t5fastcam::main()
+}
